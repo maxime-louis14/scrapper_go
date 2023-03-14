ecc20f7090acbf855a2952b0d745a9c578f0c4ce
maxime <maximelouis321@gmail.com>
2023-03-14 18:38:17 +0100

    data

--- main.go
+++ main.go
@@ -1,64 +1,43 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
-	"time"
 
 	"github.com/gocolly/colly"
 )
 
-type Product struct {
-	Titre       string
-	Image       string
-	Description string
-	Url         string
-	Ingredient  string
-	Preparation string
+type products struct {
+	Name  string `json:"name"`
+	URL   string `json:"url"`
+	Image string `json:"image"`
 }
 
-func main() {
-	c := colly.NewCollector()
-	c.SetRequestTimeout(120 * time.Second)
-	products := make([]Product, 0)
+var allProducts []products
 
-	// Callbacks
-	c.OnHTML("a.[comp card--image-top mntl-card-list-items mntl-document-card mntl-card card card--no-image]", func(e *colly.HTMLElement) {
-		e.ForEach("span.[card__title-text]", func(i int, h *colly.HTMLElement) {
-			item := Product{}
-			item.Titre = h.Text
-			item.Image = e.ChildAttr("img", "data-src")
-			item.Description = e.Attr("description")
-			item.Url = "https://www.allrecipes.com/" + e.Attr(("href"))
-			item.Preparation = e.ChildText("div.tag._dsct")
-			products = append(products, item)
-		})
-	})
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.allrecipes.com"),
+	)
 
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+		fmt.Println("Scraping:", r.URL)
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Response Code", r.StatusCode)
+		fmt.Println("Status:", r.StatusCode)
 	})
 
-	c.OnError(func(r *colly.Response, e error) {
-		fmt.Println("Got this error", e)
+	c.OnHTML("div.card__content", func(h *colly.HTMLElement) {
+		products := products{
+			Name: h.ChildText("span.card__title-text"),
+			Image: h.ChildText("a[href]"),
+		}
+		fmt.Println(products)
+		allProducts = append(allProducts, products)
 	})
 
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finshed", r.Request.URL)
-		js, err := json.MarshalIndent(products, "", "  ")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("Writing data to file")
-		if err := os.WriteFile("products.json", js, 0664); err == nil {
-			fmt.Println("Data written to file successfully")
-		}
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "nError:", err)
 	})
 
 	c.Visit("https://www.allrecipes.com/")
