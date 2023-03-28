3b5e4ae35eafd973fd9fce0aef80ba92edd4aa6e
maxime <maximelouis321@gmail.com>
2023-03-28 23:12:00 +0200

    Fonction qui récupère les noms des recettes

--- main.go
+++ main.go
@@ -28,35 +28,21 @@ func main() {
 		fmt.Println("Status:", r.StatusCode)
 	})
 
-	c.OnHTML(".three-post--home", func(h *colly.HTMLElement) {
+	// OnHTML enregistre une fonction. La fonction sera exécutée sur chaque HTML élément correspondant au paramètre
+	c.OnHTML("a.mntl-card", func(h *colly.HTMLElement) {
 		products := products{
+			URL:  h.ChildAttr("a.mntl-card-list-items", "href"),
 			Name: h.ChildText(".card__title-text"),
-			URL: h.ChildAttr(".card--image-top", "href"),
 		}
 		fmt.Println(products)
 		allProducts = append(allProducts, products)
 	})
 
-	// Ici le récupère les URL des pages
-	// c.OnHTML(".three-post__inner", func(h *colly.HTMLElement) {
-	// 	products := products{
-	// 		URL: h.ChildAttr("a.card--image-top", "href"),
-	// 	}
-	// 	fmt.Println(products)
-	// 	allProducts = append(allProducts, products)
-	// })
-	
-	// je veux récupère les data d'une autre page
-	// c.OnHTML("nav.header-nav", func(p *colly.HTMLElement) {
-	// 	nextPage := p.Request.AbsoluteURL(p.Attr("li.header-nav__list-item"))
-	// 	c.Visit(nextPage)
-	// })
-
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "nError:", err)
 	})
 
-	c.Visit("https://www.allrecipes.com/")
+	c.Visit("https://www.allrecipes.com/recipes/17562/dinner/")
 
 	content, err := json.Marshal(allProducts)
 	if err != nil {
