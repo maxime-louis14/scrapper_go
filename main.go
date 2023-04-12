fe36b8378ac8e1fe04e4e8341b9c34512a512488
maxime <maximelouis321@gmail.com>
2023-04-12 19:46:59 +0200

    nextpage

--- main.go
+++ main.go
@@ -3,22 +3,43 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/gocolly/colly"
 )
 
 type products struct {
+	recettes
 	Name string `json:"name"`
 	URL  string `json:"url"`
 }
 
+type recettes struct {
+	Descriptions string `json:"descriptions"`
+	Ingredients  string `json:"ingredients"`
+	Photos       string `json:"photos"`
+	Directions   string `json:"directions"`
+}
+
 var allProducts []products
+var allRecettes []recettes
 
 func main() {
-	c := colly.NewCollector(
-		colly.AllowedDomains("www.allrecipes.com"),
-	)
+	c := colly.NewCollector()
+	c.WithTransport(&http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   60 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Scraping:", r.URL)
@@ -38,6 +59,22 @@ func main() {
 		allProducts = append(allProducts, products)
 	})
 
+	c.OnHTML("a.mntl-card", func(p *colly.HTMLElement) {
+		nextPage := p.Request.AbsoluteURL(p.Attr("URL"))
+		c.Visit(nextPage) // Je veux aller sur une autre page.
+	})
+ 
+	c.OnHTML("article.allrecipes-article", func(h *colly.HTMLElement) {
+		recettes := recettes{
+			Descriptions: h.ChildText("p.article-subheading"),
+			Photos:       h.Attr("src"),
+			Ingredients:  h.ChildText("li.mntl-structured-ingredients__list-item"),
+			Directions:   h.ChildText("div.mntl-block"),
+		}
+		fmt.Println(recettes)
+		allRecettes = append(allRecettes, recettes)
+	})
+
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "nError:", err)
 	})
@@ -48,7 +85,7 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+
 	os.WriteFile("data.json", content, 0644)
 	fmt.Println("Total produts: ", len(allProducts))
 }
-
